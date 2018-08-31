c4edc72dd34cdd84a42ba4be2262815e16cc6a17
suberguo <suberguo@gmail.com>
2018-08-31 14:55:07 +0800

    mybatis-v2

---
+++ mybatis-v2/src/main/java/com/dds/mybatis/MapperProxy.java
@@ -0,0 +1,31 @@
+package com.dds.mybatis;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class MapperProxy implements InvocationHandler {
+
+    private SqlSession session;
+
+    public MapperProxy(SqlSession session) {
+        this.session = session;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (Object.class.equals(method.getDeclaringClass())) {
+            return method.invoke(this, args);
+        } else {
+            return this.session.selectOne(proxy.getClass().getInterfaces()[0] + "#" + method.getName(),
+                    args, method.getReturnType());
+        }
+    }
+
+    public <T> T newInstance(Class<T> clz) {
+        return (T) Proxy.newProxyInstance(clz.getClassLoader(),
+                new Class[]{clz},
+                this);
+    }
+
+}
